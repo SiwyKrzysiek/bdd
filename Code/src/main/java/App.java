d5ba51772a171d78559af4b21fd54c6fa1a5e0c3
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-12-13 18:42:17 +0100

    Przejśćie na Java 11

--- Code/src/main/java/App.java
+++ Code/src/main/java/App.java
@@ -3,12 +3,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import lombok.var;
 
 public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
-        var label = new Label("We're gona be rich!");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Label label = new Label("We're gona be rich!");
+        Scene scene = new Scene(new StackPane(label), 640, 480);
 
         primaryStage.setScene(scene);
         primaryStage.show();
