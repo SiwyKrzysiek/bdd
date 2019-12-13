218eb20756caef17cf82120ea7c10536e595cb55
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-12-13 22:26:34 +0100

    Correct login steps JUNIT ISN'T HERE!!??

--- Code/src/test/java/pw/steps/LoginScenarioSteps.java
+++ Code/src/test/java/pw/steps/LoginScenarioSteps.java
@@ -1,6 +1,37 @@
 package pw.steps;
 
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import pw.models.UserDatabase;
+
 public class LoginScenarioSteps {
 
+    private UserDatabase userDatabase;
+    private String login;
+    private String password;
 
+    @Given("User being on the login screen")
+    public void connectToDataBase() {
+        this.userDatabase = UserDatabase.getInstance();
+    }
+
+    @When("user enters existing <login>")
+    public void enterLogin(@Named("login") String login) {
+        this.login = login;
+    }
+
+    @When("user enters valid <password>")
+    public void enterPassword(@Named("password") String password) {
+        this.password = password;
+    }
+
+    @When("user presses login button")
+    public void pressLoginButton() {}
+
+    @Then("login is validated correctly")
+    public void validateLogin() {
+//        WHY THERE IS NO JUNIT HERE?!!!
+    }
 }
--- Code/src/test/java/pw/stories/login.story
+++ Code/src/test/java/pw/stories/login.story
@@ -4,11 +4,11 @@ I want to login to my banking account
 So that I can do my banking
 
 Scenario: Login with correct credentials
-Given User beeing on the login screen
-When user enters exsisting <login>
+Given User being on the login screen
+When user enters existing <login>
 And user enters valid <password>
 And user presses login button
-Then User is taken to his banking profile
+Then login is validated correctly
 
 Examples:
 |login  |password|
