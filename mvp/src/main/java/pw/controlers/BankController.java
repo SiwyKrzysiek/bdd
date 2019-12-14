5f7f946b26ea3f1abe41a0b87e928d0b6d3130ef
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-12-14 16:50:18 +0100

    Load bank fields to controller

--- mvp/src/main/java/pw/controlers/BankController.java
+++ mvp/src/main/java/pw/controlers/BankController.java
@@ -1,4 +1,28 @@
 package pw.controlers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class BankController {
+
+    @FXML
+    private TextField balanceTextField;
+
+    @FXML
+    private Spinner transferSpinner;
+
+    @FXML
+    private Button sendTransferButton;
+
+    @FXML
+    private Spinner convertFromSpinner;
+
+    @FXML
+    private ChoiceBox currencyChoiceBox;
+
+    @FXML
+    private TextField conversionResultTextField;
 }
